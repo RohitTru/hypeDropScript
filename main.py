69d0437c23475a00dd4d802b7015de040a076724
Rohit Truesdale <rohit.trues@gmail.com>
2023-05-19 22:36:17 -0500

    login function !!!needs work. Cant click on steam

--- main.py
+++ main.py
@@ -12,14 +12,22 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 #retrieve the webpage
 driver.get('https://www.hypedrop.com/en/pvp')
 
-# login button path
-login_button = driver.find_element(By.XPATH, '/html/body/cw-root/cw-header/nav/div[2]/div/cw-auth-buttons/div/button[2]')
-time.sleep(5)
-login_button.click()
 
 
+def login():
+    # login button path
+    login_button = driver.find_element(By.XPATH, '/html/body/cw-root/cw-header/nav/div[2]/div/cw-auth-buttons/div/button[2]')
+    login_button.click()
 
-time.sleep(9)
+    time.sleep(3)
+    
+    # Click on steam
+    #steam_button =  driver.find_element(By.NAME, 'Steam')
+    #steam_button.click()
+
+
+time.sleep(3)
+
 
 driver.close()
 driver.quit()

3ff83f7b5c92e7f2da85e23b477a2fe26311087d
Rohit Truesdale <rohit.trues@gmail.com>
2023-05-19 21:56:32 -0500

    little organization and included proper webpage

--- main.py
+++ main.py
@@ -3,9 +3,35 @@ from selenium import webdriver
 import os
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+from creds import username, password
+
+#Selinium setup
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-driver.get('https://www.youtube.com/@MusicbyChoice')
+#retrieve the webpage
+driver.get('https://www.hypedrop.com/en/pvp')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 time.sleep(30)
 
